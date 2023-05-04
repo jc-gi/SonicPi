6d5213fd767c71825764ae1cbab9054600d5e1fd
jc-gi <juancarlos.gonzalez@upslp.edu.mx>
2023-05-03 18:57:39 -0600

    Primer ejemplo Sonic Pi
    Uso de play & sleep

---
+++ Ejemplo1.rb
@@ -0,0 +1,26 @@
+#Play lo que genera es la ejecucion de un sonido
+play 60
+# 60 la nota musical en formato midi
+play :c4 #:c4 es la nota musical en su formato tradicional
+sleep 1
+#sleep indica a Sonic Pi el tiempo de espera para generar el siguiente sonido
+
+#Notas musicales
+play 72
+sleep 0.25
+play 76
+sleep 0.25#0.5
+play 76
+sleep 0.25
+play 72
+sleep 0.5
+play 83
+sleep 0.25
+play 74
+sleep 0.25
+play 83
+sleep 0.25
+play 79
+sleep 0.25
+play 84
+sleep 0.25
